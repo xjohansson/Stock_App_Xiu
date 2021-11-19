9ddd8612aeb1ed12e9397bc856d34cfdfce90bd3
xjohansson <76945246+xjohansson@users.noreply.github.com>
2021-11-19 12:27:27 +0100

    ok

--- L5-stockdash_teacher_alongs/L5.2-dashboard/load_data.py
+++ L5-stockdash_teacher_alongs/L5.2-dashboard/load_data.py
@@ -1,4 +1,7 @@
 import pandas as pd
+df=pd.read_csv("../data/AAPL_TIME_SERIES_DAILY.csv")
+
+print(df.head())
 
 class StockDataLocal:
     """Class method to get and process local stock data"""
@@ -7,8 +10,8 @@ class StockDataLocal:
         self._data_folder_path = data_folder_path
 
     def stock_dataframe(self, stockname: str) -> list:
+        
         """
-
         Returns:
             list of two dataframes, one for daily time series, one for interdaily
         """
--- L5-stockdash_teacher_alongs/L5.2-dashboard/load_dataXIU.py
+++ L5-stockdash_teacher_alongs/L5.2-dashboard/load_dataXIU.py
@@ -1,18 +1,5 @@
 import pandas as pd 
-df=pd.read_csv("..\data\AAPL_TIME_SERIES_DAILY.csv")
+df=pd.read_csv("../data/AAPL_TIME_SERIES_DAILY.csv")
+#df=pd.read_csv("..\data\AAPL_TIME_SERIES_DAILY.csv")
 print(df.head())
 
-class StockDataLocal:
-    """Class method to get and process local stock data"""
-
-    def __init__(self, data_folder_path: str = "../data/") -> None:
-        self._data_folder_path = data_folder_path
-
-    def stock_dataframe(self, stockname: str) -> list:
-        """Returns:list of two dataframes, one for daily time series, one for interdaily """
-        stock_df_list = []
-
-        for path_ending in ["_TIME_SERIES_DAILY.csv", "_TIME_SERIES_INTRADAY_EXTENDED.csv"]:
-            path = self._data_folder_path + stockname + path_ending
-            print(path)
-            
