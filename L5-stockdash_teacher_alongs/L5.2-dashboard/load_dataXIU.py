905cebf701a396dfad26e224bec72ba67e11caac
xjohansson <76945246+xjohansson@users.noreply.github.com>
2021-11-19 12:13:41 +0100

    move data under lecture 5.2

--- L5-stockdash_teacher_alongs/L5.2-dashboard/load_dataXIU.py
+++ L5-stockdash_teacher_alongs/L5.2-dashboard/load_dataXIU.py
@@ -1,5 +1,18 @@
 import pandas as pd 
-df=pd.read_csv("L5-stockdash_teacher_alongs\data\AAPL_TIME_SERIES_DAILY.csv")
+df=pd.read_csv("..\data\AAPL_TIME_SERIES_DAILY.csv")
 print(df.head())
 
-#C:\Users\Xiu\Documents\GitHub\Stock_app_Xiu\L5-stockdash_teacher_alongs\data
+class StockDataLocal:
+    """Class method to get and process local stock data"""
+
+    def __init__(self, data_folder_path: str = "../data/") -> None:
+        self._data_folder_path = data_folder_path
+
+    def stock_dataframe(self, stockname: str) -> list:
+        """Returns:list of two dataframes, one for daily time series, one for interdaily """
+        stock_df_list = []
+
+        for path_ending in ["_TIME_SERIES_DAILY.csv", "_TIME_SERIES_INTRADAY_EXTENDED.csv"]:
+            path = self._data_folder_path + stockname + path_ending
+            print(path)
+            
